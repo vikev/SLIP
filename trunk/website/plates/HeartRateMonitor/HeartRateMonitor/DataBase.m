afb3cf54ae6dda8ca0e4c6980afbfdfe40d98801
Konstantinos Chartomatzis <constchart@gmail.com>
2014-10-13 18:45:51 +0100

    Prospeckz to sqlite3 through bluetooth

---
+++ trunk/website/plates/HeartRateMonitor/HeartRateMonitor/DataBase.m
@@ -0,0 +1,11 @@
+//
+//  DataBase.m
+//  HeartRateMonitor
+//
+//  Created by Konstantinos Chartomatzis on 12/10/2014.
+//  Copyright (c) 2014 Apple Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
