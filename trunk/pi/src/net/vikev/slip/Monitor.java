6a7aa6ca14e9b05e348b3223ccdbf1c92f488163
vikev <lubomirvikev@gmail.com>
2014-11-20 03:08:32 +0000

    BLEHeartRateMonitor macs retrieved from web service

--- trunk/pi/src/net/vikev/slip/Monitor.java
+++ trunk/pi/src/net/vikev/slip/Monitor.java
@@ -1,16 +1,50 @@
 package net.vikev.slip;
 
-import net.vikev.slip.ble.HearthRateMonitor;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import net.vikev.slip.ble.ScaleMonitor;
 import net.vikev.slip.utils.WebClient;
 import net.vikev.slip.utils.WebClientImpl;
 
 public class Monitor {
+    private static final int MACS_REFRESH_INTERVAL = 10000;
     private WebClient webClient = WebClientImpl.getInstance();
+    private Map<String, ScaleMonitor> monitors = new HashMap<>();
+    private List<String> macs;
 
     public void start() {
-        for (String mac : webClient.getMACs()) {
-            System.out.println("Starting connection to " + mac);
-            new HearthRateMonitor(mac);
+        while (true) {
+            macs = webClient.getMACs();
+
+            for (String mac : macs) {
+                updateMonitor(mac);
+            }
+
+            removeObsolateMonitors();
+
+            try {
+                Thread.sleep(MACS_REFRESH_INTERVAL);
+            } catch (InterruptedException e) {
+                // should not happen. Even if it happens it is not a problem.
+            }
+        }
+    }
+
+    private void removeObsolateMonitors() {
+        List<String> obsolateMacs = new ArrayList<String>(monitors.keySet());
+        obsolateMacs.removeAll(macs);
+
+        for (String mac : obsolateMacs) {
+            monitors.remove(mac).terminate();
+        }
+    }
+
+    private void updateMonitor(String mac) {
+        if (!monitors.keySet().contains(mac)) {
+            monitors.put(mac, new ScaleMonitor(mac));
         }
     }
 
--- trunk/pi/src/net/vikev/slip/utils/WebClientImpl.java
+++ trunk/pi/src/net/vikev/slip/utils/WebClientImpl.java
@@ -1,17 +1,27 @@
 package net.vikev.slip.utils;
 
-import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class WebClientImpl implements WebClient {
     private static WebClientImpl webClient;
     private static String USER_AGENT = "BLEMonitor";
     private static String URL = "http://localhost:8000/";
+    private List<String> macs;
 
     private WebClientImpl() {
         // no instantiation
@@ -35,23 +45,32 @@ public class WebClientImpl implements WebClient {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        // String url = URL + "/api/mac/" + deviceMac + "/?reading=" + value;
-        // try {
-        // new URL(url).openConnection();
-        // } catch (IOException e) {
-        // // TODO Auto-generated catch block
-        // e.printStackTrace();
-        // }
     }
 
     @Override
     public List<String> getMACs() {
-        // TODO Auto-generated method stub
-        List<String> macs = new ArrayList<>();
-        macs.add("DA:0F:85:E2:57:19");
-        macs.add("C9:9A:51:A3:C3:D2");
+        getAndParseMacsFromWebServer();
+        System.out.println(macs);
+        
+        return this.macs;
+    }
 
-        return macs;
+    private void getAndParseMacsFromWebServer() {
+        try {
+            List<String> macs = new ArrayList<>();
+            JSONObject serverReply;
+            serverReply = new JSONObject(httpGET(URL + "api/allmac/"));
+            JSONArray macsArray = serverReply.getJSONArray("addresses");
+
+            for (int i = 0; i < macsArray.length(); i++) {
+                macs.add(macsArray.getString(i));
+            }
+
+            this.macs = macs;
+        } catch (JSONException | IllegalStateException | IOException e) {
+            // If can't connect it won't change the values from the previous run
+            // so that is not a problem.
+        }
     }
 
     private void sendGet(String mac, Short value) throws Exception {
@@ -59,31 +78,31 @@ public class WebClientImpl implements WebClient {
         URL obj = new URL(url);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
-        // optional default is GET
         con.setRequestMethod("GET");
 
         // add request header
         con.setRequestProperty("User-Agent", USER_AGENT);
 
-        // con.setRequestProperty("mac", mac);
-        // con.setRequestProperty("value", value);
-
         int responseCode = con.getResponseCode();
-        System.out.println("\nSending 'GET' request to URL : " + URL);
         System.out.println("Response Code : " + responseCode);
+    }
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer response = new StringBuffer();
+    public String httpGET(String URL) throws IOException, IllegalStateException {
+        HttpClient httpclient = HttpClientBuilder.create().build();
+        HttpResponse response = httpclient.execute(new HttpGet(URL));
+        StatusLine statusLine = response.getStatusLine();
 
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
+        if (statusLine.getStatusCode() == HttpStatus.SC_OK) {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            response.getEntity().writeTo(out);
+            out.close();
+            response.getEntity().getContent().close();
+
+            return out.toString();
+        } else {
+            // Closes the connection.
+            response.getEntity().getContent().close();
+            throw new IOException(statusLine.getReasonPhrase());
         }
-
-        in.close();
-
-        // print result
-        // System.out.println(response.toString());
-
     }
 }
