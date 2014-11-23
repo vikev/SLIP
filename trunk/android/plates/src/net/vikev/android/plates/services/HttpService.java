df8ba53e4c748004ecae7ca67638645a1616ada2
vikev <lubomirvikev@gmail.com>
2014-11-23 20:12:13 +0000

    bug fixes

--- trunk/android/plates/src/net/vikev/android/plates/services/HttpService.java
+++ trunk/android/plates/src/net/vikev/android/plates/services/HttpService.java
@@ -1,7 +1,8 @@
 package net.vikev.android.plates.services;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 public interface HttpService {
-    String httpGET(String URL) throws IOException;
+    String httpGET(String URL) throws IOException, IllegalStateException, URISyntaxException;
 }
